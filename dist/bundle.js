4288341640f57acd44916f9d3aab9405f73aeb65
Cinquin Andy <cinquin.andy@gmail.com>
2021-07-06 15:26:45 +0200

    On scroll change the background and add blur -> responsive

--- dist/bundle.js
+++ dist/bundle.js
@@ -15,7 +15,7 @@
   \************************/
 /***/ (() => {
 
-eval("var menuIcon = document.getElementById(\"plate2\");\nvar navLinks = document.getElementById(\"navLinks\");\n\nif (navigator.userAgent.indexOf(\"Firefox\") != -1) {\n  menuIcon.classList.remove(\"burger\");\n}\n\nmenuIcon.addEventListener(\"click\", function () {\n  navLinks.classList.toggle(\"_yd_fade_visible\");\n  navLinks.classList.toggle(\"_yd_fade_invisible\");\n});\n\n//# sourceURL=webpack://base/./src/js/main.js?");
+eval("var menuIcon = document.getElementById(\"plate2\");\nvar navLinks = document.getElementById(\"navLinks\");\nvar nav = document.getElementById(\"nav\");\n\nif (navigator.userAgent.indexOf(\"Firefox\") != -1) {\n  menuIcon.classList.remove(\"burger\");\n}\n\nmenuIcon.addEventListener(\"click\", function () {\n  navLinks.classList.toggle(\"_yd_fade_visible\");\n  navLinks.classList.toggle(\"_yd_fade_invisible\");\n}); // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size\n\nwindow.onscroll = function () {\n  scrollFunction();\n};\n\nvar flag = false;\n\nfunction scrollFunction() {\n  console.log(document.body.scrollTop);\n  console.log(document.documentElement.scrollTop);\n\n  if (flag === false) {\n    if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {\n      nav.classList.toggle(\"scrolled\");\n      flag = true;\n    }\n  }\n}\n\n//# sourceURL=webpack://base/./src/js/main.js?");
 
 /***/ })
 
