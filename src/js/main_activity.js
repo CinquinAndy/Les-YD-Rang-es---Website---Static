ed93a1a56c5c214057c10b5a358ba40a3bf0b567
Cinquin Andy <cinquin.andy@gmail.com>
2021-07-09 13:38:22 +0200

    clean code, fix bug

--- src/js/main_activity.js
+++ src/js/main_activity.js
@@ -11,31 +11,3 @@ menuIcon.addEventListener("click", function () {
     navLinks.classList.toggle("_yd_fade_visible");
     navLinks.classList.toggle("_yd_fade_invisible");
 });
-
-// document.addEventListener( "DOMContentLoaded", function () {
-//     var secondarySlider = new Splide( "#secondary-slider", {
-//         fixedWidth  : 100,
-//         height      : 60,
-//         gap         : 10,
-//         cover       : true,
-//         isNavigation: true,
-//         pagination: false,
-//         focus       : "center",
-//         breakpoints : {
-//             "600": {
-//                 fixedWidth: 66,
-//                 height    : 40,
-//             }
-//         },
-//     } ).mount();
-//
-//     var primarySlider = new Splide( "#primary-slider", {
-//         type       : "fade",
-//         heightRatio: 0.5,
-//         pagination : false,
-//         arrows     : false,
-//         cover      : true,
-//     } ); // do not call mount() here.
-//
-//     primarySlider.sync( secondarySlider ).mount();
-// } );
