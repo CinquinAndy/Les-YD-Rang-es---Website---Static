117fd45507c0505e6b2c65dccf9bee0656305927
Cinquin Andy <cinquin.andy@gmail.com>
2021-07-13 09:41:34 +0200

    Finish prestation page - responsive design - Starting the last one - Blog général page !

--- dist/mainActivity.bundle.js
+++ dist/mainActivity.bundle.js
@@ -1,1 +1,32 @@
-(()=>{var e=document.getElementById("plate2"),t=document.getElementById("navLinks"),n=document.getElementById("_btn_arrow_left"),d=document.getElementById("_btn_arrow_right"),c=[document.getElementById("id1"),document.getElementById("id2"),document.getElementById("id3"),document.getElementById("id4"),document.getElementById("id5")],i=1;function l(){for(var e=0;e<5;e++)c[e].checked=!1}-1!=navigator.userAgent.indexOf("Firefox")&&e.classList.remove("burger"),e.addEventListener("click",(function(){t.classList.toggle("_yd_fade_visible"),t.classList.toggle("_yd_fade_invisible")})),d.addEventListener("click",(function(){1!==i&&(l(),i--,c[i-1].checked=!0)})),n.addEventListener("click",(function(){5!==i&&(l(),i++,c[i-1].checked=!0)}))})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main_activity.js":
+/*!*********************************!*\
+  !*** ./src/js/main_activity.js ***!
+  \*********************************/
+/***/ (() => {
+
+eval("var menuIcon = document.getElementById(\"plate2\");\nvar navLinks = document.getElementById(\"navLinks\");\n\nvar _btn_arrow_left = document.getElementById(\"_btn_arrow_left\");\n\nvar _btn_arrow_right = document.getElementById(\"_btn_arrow_right\");\n\nvar slide = [document.getElementById(\"id1\"), document.getElementById(\"id2\"), document.getElementById(\"id3\"), document.getElementById(\"id4\"), document.getElementById(\"id5\")];\nvar compteur = 1;\n\nif (navigator.userAgent.indexOf(\"Firefox\") != -1) {\n  menuIcon.classList.remove(\"burger\");\n}\n\nmenuIcon.addEventListener(\"click\", function () {\n  navLinks.classList.toggle(\"_yd_fade_visible\");\n  navLinks.classList.toggle(\"_yd_fade_invisible\");\n});\n\nfunction reset_check() {\n  for (var i = 0; i < 5; i++) {\n    slide[i].checked = false;\n  }\n}\n\n_btn_arrow_right.addEventListener(\"click\", function () {\n  if (compteur !== 1) {\n    reset_check();\n    compteur--;\n    slide[compteur - 1].checked = true;\n  }\n});\n\n_btn_arrow_left.addEventListener(\"click\", function () {\n  if (compteur !== 5) {\n    reset_check();\n    compteur++;\n    slide[compteur - 1].checked = true;\n  }\n});\n\n//# sourceURL=webpack://base/./src/js/main_activity.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main_activity.js"]();
+/******/ 	
+/******/ })()
+;
--- src/js/main_prestation.js
+++ src/js/main_prestation.js
@@ -1,5 +1,6 @@
 let menuIcon = document.getElementById("plate2");
 let navLinks = document.getElementById("navLinks");
+let nav = document.getElementById("nav");
 
 if (navigator.userAgent.indexOf("Firefox") != -1) {
     menuIcon.classList.remove("burger");
@@ -9,3 +10,19 @@ menuIcon.addEventListener("click", function () {
     navLinks.classList.toggle("_yd_fade_visible");
     navLinks.classList.toggle("_yd_fade_invisible");
 });
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function () {
+    scrollFunction();
+};
+
+var flag = false;
+
+function scrollFunction() {
+    if (flag === false) {
+        if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
+            nav.classList.toggle("scrolled");
+            flag = true;
+        }
+    }
+}
